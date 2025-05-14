9caebd28948135a9d061d87e6c9cf8f873db5fd8
Bandhu0014 <stoneheartbreaker007@gmail.com>
2025-05-14 15:02:31 +0530

    Added schema.yml and source table for employees

--- models/staging/stg_employees.sql
+++ models/staging/stg_employees.sql
@@ -1,1 +1,9 @@
-SELECT * FROM raw.hr_employees;
+{{ config(materialized='view') }}
+
+SELECT
+    employee_id,
+    first_name,
+    last_name,
+    department,
+    salary
+FROM {{ source('hr', 'employees') }}
